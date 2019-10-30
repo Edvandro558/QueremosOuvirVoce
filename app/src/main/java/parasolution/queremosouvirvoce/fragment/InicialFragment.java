4d682ed7c74f49c091e9497e8ab8dd5d57dd3ef3
wellesleyjs <wellesley@hotmail.com.br>
2019-10-30 19:42:34 -0300

    deu erro ao chamar o fragmenteTesteB, coloquei para chamar o categorias fragment.

--- app/src/main/java/parasolution/queremosouvirvoce/fragment/InicialFragment.java
+++ app/src/main/java/parasolution/queremosouvirvoce/fragment/InicialFragment.java
@@ -68,7 +68,7 @@ public class InicialFragment extends Fragment {
                 {
                     //chamando o proximo fragment
                     fragmentTransaction = getFragmentManager();
-                    fragmentTransaction.beginTransaction().replace(R.id.content_fragment, new CategoriasTesteBFragment()).commit();
+                    fragmentTransaction.beginTransaction().replace(R.id.content_fragment, new CategoriasFragment()).commit();
                     Toast.makeText(getContext(),"Brasil", Toast.LENGTH_LONG).show();
                 }
             }
@@ -80,8 +80,8 @@ public class InicialFragment extends Fragment {
                 if(((ToggleButton)v).isChecked())
                 {
                     //chamando o proximo fragment
-                    fragmentTransaction = getFragmentManager();
-                    fragmentTransaction.beginTransaction().replace(R.id.content_fragment, new CategoriasTesteBFragment()).commit();
+                    fragmentTransaction = getFragmentManager();                         //CategoriasTesteBFragment
+                    fragmentTransaction.beginTransaction().replace(R.id.content_fragment, new CategoriasFragment()).commit();
                     Toast.makeText(getContext(),"Espanhol", Toast.LENGTH_LONG).show();
                 }
             }
